ac2ba49da6455467d67058fa0abb9f3890603e06
Abduvohidov-a <sardorabduvohidov06@gmail.com>
2025-04-03 03:36:30 +0100

    create class Board, add field size, create placing ship function

--- Battleship.py
+++ Battleship.py
@@ -1,4 +1,11 @@
 import tkinter as tk
-import random
+import random   # importing necessary modules
 
-print("hello world")
+class Board: # creating class Board which include information ship placing, ship hitting, and ship sunking
+    def __init__(self, size=10):
+        self.size = size # Field size
+        self.grid = [[" "] * size for _ in range(size)] # creating empty field
+
+    def place_ship(self, ship): # method placing ship in field
+        for x,y in ship.coords:
+            self.grid[y][x] = ship.symbol # set the ship symbol to the coordinates on the field
