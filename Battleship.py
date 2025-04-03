96873447741ae3fdd393a284aacc7fa3c20db4d2
Abduvohidov-a <sardorabduvohidov06@gmail.com>
2025-04-03 04:32:06 +0100

    add logging for write game results

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,14 @@
 import tkinter as tk
-import random   # importing necessary modules
+import random # importing necessary modules
+import logging
+from datetime import datetime
+
+logging.basicConfig( # set up settings for logging, writing game results in file
+    filename="battleship_results.log", # creating file
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S" # set up time
+)
 
 class Board: # creating class Board which include information ship placing, ship hitting, and ship sunking
     def __init__(self, size=10):
@@ -9,3 +18,16 @@ class Board: # creating class Board which include information ship placing, ship
     def place_ship(self, ship): # method placing ship in field
         for x,y in ship.coords:
             self.grid[y][x] = ship.symbol # set the ship symbol to the coordinates on the field
+
+    def hit(self, x, y): # methods for hand
+        if self.grid[y][x] == " ":
+            self.grid[y][x] = "."
+            return False
+        elif self.grid[y][x] in {".", "X"}:
+            return False
+        else:
+            self.grid[y][x] = "X"
+            return True
+
+    def all_ships_sunk(self):
+        return all(cell in {"X", " ", "."} for row in self.grid for cell in row)
